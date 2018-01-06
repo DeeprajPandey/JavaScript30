aa17c87fbcd331cdc4aa3ff95e220bc90452bf21
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2018-01-06 22:20:07 +0530

    Added the playSound function

--- day1/jsDrumKit/assets/main.js
+++ day1/jsDrumKit/assets/main.js
@@ -1,18 +1,23 @@
-window.addEventListener('keydown', function(e){
+function playSound(e) {
 	//const audio = document.querySelector('audio[data-key=65]') //single quotes would make data-key a variable that lives within the function
-	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+	const audio = document.querySelector(`audio[data-key="${e.keyCode}"`)
 	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
-	if(!audio) return
+
+	if(!audio)
+		return
 
 	audio.currentTime = 0
 	audio.play()
 	key.classList.add('playing')
-})
+}
 
 function removeTransition(e) {
-	if (e.propertyName !== 'transform') return //return if it's not a transform
+	if (e.propertyName !== 'transform')
+		return //return if it's not a transform
 	this.classList.remove('playing')
 }
 
-const keys = document.querySelectorAll('.key')         
+const keys = Array.from(document.querySelectorAll('.key'))         
 keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+
+window.addEventListener('keydown', playSound)
