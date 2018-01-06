9c9ed0cb624906e82015f52efbbaa405fd540a2f
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2018-01-06 21:29:55 +0530

    Set up listeners for transitionend

--- day1/jsDrumKit/assets/main.js
+++ day1/jsDrumKit/assets/main.js
@@ -1,3 +1,17 @@
 window.addEventListener('keydown', function(e){
+	//const audio = document.querySelector('audio[data-key=65]') //single quotes would make data-key a variable that lives within the function
+	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+	if(!audio) return
+
+	audio.currentTime = 0
+	audio.play()
+	key.classList.add('playing')
+})
+
+function removeTransition(e) {
 	console.log(e);
-});
+}
+
+const keys = document.querySelectorAll('.key')         
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
